141cf53f7a3cbfc4c321cc64b34f4511e8cebc41
LAPTOP-NDTF2MRT\jiank <cjk7072@closeli.cn>
2022-09-04 12:49:26 +0800

    add deploy file

--- config/config.default.js
+++ config/config.default.js
@@ -51,6 +51,14 @@ module.exports = appInfo => {
     // {string|Array} allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
   }
 
+  config.cluster = {
+    listen: {
+      port: 7001,
+      hostname: '0.0.0.0' // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+      // path: '/var/run/egg.sock',
+    }
+  }
+
   return {
     ...config,
     ...userConfig
